431c95e93f385496e42233273d668a7273d2a47a
bguillou <b.guillou@inextenso.digital>
2019-03-07 10:13:48 +0100

    Fix createBody

--- release/helpers/createBody.js
+++ release/helpers/createBody.js
@@ -1,8 +1,1 @@
-module.exports = ({ header, body }) =>
-  new Buffer(
-    `
-${header}
-${body}
-`,
-    'binary',
-  )
+module.exports = ({ header, body }) => new Buffer(`${header} ${body}`, 'binary')
