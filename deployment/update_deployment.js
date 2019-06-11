a6b16574a9f39bdfd2e3e8442f4c70568ae5f5c4
Sébastien HOUZÉ <s.houze@inextenso.digital>
2019-06-11 11:17:19 +0200

    fix: non prod deployment status update to success

--- deployment/update_deployment.js
+++ deployment/update_deployment.js
@@ -4,6 +4,12 @@ const api = require('./api')
 module.exports = async () => {
   const event = await context.readEvent()
   if (event.deployment) {
-    await api.createDeploymentStatus(event.deployment.id, process.argv[3])
+    const response = await api.createDeploymentStatus(event.deployment.id, process.argv[3])
+    process.exit(0)
+  }
+
+  const deploymentEvent = await context.readJSON('deployment')
+  if (deploymentEvent.deployment) {
+    const response = await api.createDeploymentStatus(deploymentEvent.deployment.id, process.argv[3])
   }
 }
