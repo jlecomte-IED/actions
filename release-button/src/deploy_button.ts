68289e86376dc50fa921f8b2e67e64886af20c1f
Vincent Bouvier <vincent.bouvier1987@gmail.com>
2019-08-19 14:34:01 +0200

    add action.yml
    use node12
    
    install packages fist
    
    use typescript
    
    fix main
    
    test with node_module for dependencies
    
    fix main
    
    add missing dependencies
    
    add toolkit
    
    add node_module
    
    fix deploy-id input
    
    rebuild new version
    
    remove useless files

---
+++ release-button/src/deploy_button.ts
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+/* eslint-disable global-require, import/no-dynamic-require */
+import * as core from '@actions/core'
+import crypto from 'crypto'
+import { stringify } from 'querystring'
+
+function generateButton(deployId: string) {
+  const { PRIVATE_KEY, GITHUB_REPOSITORY, GITHUB_REF } = process.env
+
+  if (!PRIVATE_KEY || !GITHUB_REPOSITORY || !GITHUB_REF) {
+    console.log(
+      'Environment variable PRIVATE_KEY, GITHUB_REPOSITORY and GITHUB_REF are required.',
+    )
+    process.exit(1)
+    return
+  }
+
+  const [owner, repo] = GITHUB_REPOSITORY.split('/')
+  const [, , refName] = GITHUB_REF.split('/')
+
+  const sign = crypto.createSign('RSA-SHA256')
+
+  const query = stringify({
+    owner,
+    repo,
+    deploy: deployId,
+    tag: refName,
+    sign: sign.sign(PRIVATE_KEY, 'hex'),
+  })
+
+  const url = `https://auto-deploy.inextenso.io/deploy?${query}`
+  const img =
+    'https://img.shields.io/badge/Deploy%20to-Production-orange.svg?style=for-the-badge'
+
+  console.log(`[![Deploy to prod](${img})](${url})`)
+}
+
+const action = core.getInput('action', { required: true })
+
+switch (action) {
+  case 'generate-button':
+    const deployId = core.getInput('deploy-id', { required: true })
+    generateButton(deployId)
+    break
+
+  default:
+    process.exit(1)
+}
