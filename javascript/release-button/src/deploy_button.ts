cf2d81b431456a2f15a27ee3d4975d9b42db788c
Rémy Ntshaykolo <54712916+RemyNtshaykolo@users.noreply.github.com>
2021-01-05 13:41:13 +0100

    Update javascript/release-button/src/deploy_button.ts
    Co-authored-by: Vincent Bouvier <vincent.bouvier1987@gmail.com>

--- javascript/release-button/src/deploy_button.ts
+++ javascript/release-button/src/deploy_button.ts
@@ -76,9 +76,9 @@ function generateButton(
 
 try {
   const deployId = core.getInput('deploy_id')
-  const deployModel = core.getInput('deploy_model')
+  const deployType = core.getInput('deploy_type')
   const deployEnv = core.getInput('deploy_env')
-  generateButton(deployId, deployModel, deployEnv)
+  generateButton(deployId, deployType, deployEnv)
 } catch (err) {
   core.setFailed(err)
   process.exit(1)
